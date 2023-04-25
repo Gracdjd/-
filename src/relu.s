b77fe1d346cf3351a7ee20c6c018ab24d9c57618
Gracdjd <ziwenwang950@gmail.com>
2023-04-25 11:17:54 +0800

    i am going to use a new version

--- src/relu.s
+++ src/relu.s
@@ -15,19 +15,28 @@
 relu:
     # Prologue
 
+    li t0, 1
+    blt a1, t0, exit
+
+    lw t1, 0(a0)
+
 
 loop_start:
-    
-
-
-
-
-
-
-
+    blt t1, zero, loop_continue
+    addi a0, a0, 4
+    lw t1, 0(a0)
+    j loop_start
+#用来处理 小于0
 loop_continue:
-
-
+    sw zero, 0(a0)
+    addi a0, a0, 4
+    
+    addi t0, t0, 1
+    lw t1, 0(a0)
+    beq t0, a1, loop_end
+    j loop_start
+exit:
+    li a1, 32
 
 loop_end:
 
