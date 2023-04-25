486ab5defb0a7ea3f72c45907030a4f74999b411
Gracdjd <ziwenwang950@gmail.com>
2023-04-25 11:51:55 +0800

    fixed

--- src/relu.s
+++ src/relu.s
@@ -18,30 +18,23 @@ relu:
     li t0, 1
     blt a1, t0, exit
 
-    lw t1, 0(a0)
-
+    li t2, 0
 
 loop_start:
+    slli t3, t2, 2
+    add t3, a0, t3
+    lw t1, 0(t3)
     blt t1, zero, loop_continue
-    addi a0, a0, 4
-    lw t1, 0(a0)
-    j loop_start
-#用来处理 小于0
-loop_continue:
-    sw zero, 0(a0)
-    addi a0, a0, 4
-    
-    addi t0, t0, 1
-    lw t1, 0(a0)
-    beq t0, a1, loop_end
-    j loop_start
-exit:
-    li a1, 32
 
 loop_end:
+    addi t2, t2, 1
+    bge t2, a1, exit
+    j loop_start
 
+loop_continue:
+    sw zero, 0(t3)
+    j loop_end
 
+exit:
     # Epilogue
-
-    
 	ret
